e53bba99400c0101ea847386e4f5c5032b9bd9b7
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-06-18 16:33:27 +0530

    Update Cards.jsx

--- src/components/main/Cards.jsx
+++ src/components/main/Cards.jsx
@@ -3,10 +3,18 @@ import Link from "next/link"
 
 function Cards({ title, difficulty, image, link }) {
   return (
-    <div className="bg-lightGray grid gap-2 px-4 py-4 rounded-md ">
-      <Image src={image} alt="" width={400} height={400} className="hover:scale-105"></Image>
+    <div className="bg-lightGray grid gap-2 px-4 py-4 rounded-md  ">
+      <Image
+        src={image}
+        alt=""
+        width={400}
+        height={400}
+        className="hover:scale-105  duration-300"
+      ></Image>
       <Link href={link}>
-        <h1 className="text-sm sm:text-base md:text-lg font-semibold">{title}</h1>
+        <h1 className="text-sm sm:text-base md:text-lg font-semibold">
+          {title}
+        </h1>
       </Link>
       <p>{difficulty}</p>
     </div>
