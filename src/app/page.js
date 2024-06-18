f496cafc66e32612fae874babc853732d28c0235
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-06-18 17:12:27 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,10 +6,13 @@ export default function Home() {
   return (
     <main className="flex flex-col mx-auto gap-8">
       <Header />
-      <div>
-        <h1 className="text-xl md:text-2xl lg:text-3xl text-almostWhite font-extrabold tracking-wide text-center">
+      <div className="p-2 sm:px-4 md:px-8 lg:px-16 xl:px-32 grid gap-2 text-almostWhite">
+        <h1 className="text-xl md:text-2xl lg:text-3xl  font-extrabold tracking-wide ">
           All Challanges
         </h1>
+        <p className="text-xs md:text-sm lg:text-base font-medium">
+          Challanges list from Frontend Mentor which I&apos;ve completed.
+        </p>
       </div>
       <CardSection></CardSection>
     </main>
