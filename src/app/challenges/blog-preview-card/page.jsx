a02cef4b25dd55cc308e73f35f0439345bd0f65f
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-06-20 13:17:34 +0530

    Update page.jsx
    blog preview card style still some remained

--- src/app/challenges/blog-preview-card/page.jsx
+++ src/app/challenges/blog-preview-card/page.jsx
@@ -2,26 +2,31 @@ import Image from "next/image"
 
 const page = () => {
   return (
-    <main className=" flex items-center justify-center h-screen bg-bpc-yellow font-Figtree">
-      <div className="w-[340px] h-[522px] bg-bpc-pure-white border-bpc-black rounded-2xl border-[1px] flex flex-col items-center gap-6 shadow-custom">
+    <main className=" flex items-center justify-center min-h-screen bg-bpc-yellow font-Figtree">
+      <div className="max-w-[340px] h-fit bg-bpc-pure-white border-bpc-black rounded-2xl border-[1px] flex flex-col items-center gap-6 shadow-custom">
         <div class="mt-6">
           <Image
             src="/blog-preview-card/illustration-article.svg"
             alt="blog-img"
             width={300}
             height={300}
+            className=" rounded-2xl max-w-[300px]"
           />
         </div>
-        <div class="text-container">
-          <span class="category w">Category</span>
-          <p class="date">Published 21 Dec 2024</p>
-          <h1 class="heading">HTML & CSS foundations</h1>
-          <p class="text">
+        <div class="max-w-[336px] max-h-[194px] flex flex-col  gap-4 ml-8 text-[15px]">
+          <span class=" bg-bpc-yellow p-2.5 font-extrabold  rounded-[0.2rem] w-fit">
+            Category
+          </span>
+          <p class="font-medium">Published 21 Dec 2024</p>
+          <h1 class="font-extrabold text-[1.4rem] hover:text-bpc-yellow">
+            HTML & CSS foundations
+          </h1>
+          <p class="text-bpc-grey">
             These languages are the backbone of every website, defining
             structure, content, and presentation.
           </p>
         </div>
-        <div class="profile">
+        <div class="flex items-center font-extrabold gap-4 mb-4 -ml-40 text-[15px]">
           <Image
             src="/blog-preview-card/image-avatar.webp"
             alt="image-avatar"
