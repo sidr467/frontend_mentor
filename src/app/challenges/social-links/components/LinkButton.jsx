8e62d6484a2c4ad7583ed5ec28f374828063605c
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-06-26 16:42:57 +0530

    converted  repetitive linksbutton in component
    In social links challenge

---
+++ src/app/challenges/social-links/components/LinkButton.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+const LinkButton = ({name}) => {
+  return (
+    <Link
+      href="#"
+      className=" block bg-sc-Grey w-[310px] p-4 text-center text-[0.9rem] font-white rounded-xl font-bold hover:bg-sc-Green hover:text-sc-OffBlack"
+    >
+      {name}
+    </Link>
+  )
+}
+
+export default LinkButton
--- src/app/challenges/social-links/page.jsx
+++ src/app/challenges/social-links/page.jsx
@@ -1,11 +1,32 @@
+"use client"
+
 import Image from "next/image"
 import Link from "next/link"
+import LinkButton from "./components/LinkButton"
+
+const Links = [
+  {
+    name: "Github",
+  },
+  {
+    name: "Frontend Mentor",
+  },
+  {
+    name: "Twitter",
+  },
+  {
+    name: "LinkedIN",
+  },
+  {
+    name: "Instagram",
+  },
+]
 
 const page = () => {
   return (
     <main className=" font-sl-inter flex items-center justify-center min-h-screen bg-sc-OffBlack text-sc-White">
       <div className="bg-sc-DarkGrey w-[350px] h-fit rounded-2xl flex flex-col items-center gap-4">
-        <div class="img-container">
+        <div className="">
           <Image
             src="/social-links/avatar-jessica.jpeg"
             alt="avatar-jessica"
@@ -14,46 +35,21 @@ const page = () => {
             className="rounded-full mt-8"
           />
         </div>
-        <div class="text-center text-[15px]">
-          <h1 class="font-semibold text-3xl">Jessica Randall</h1>
-          <p class="mt-[0.6rem] font-bold text-[0.8rem] text-sc-Green mb-[1.4rem]">
+        <div className="text-center text-[15px]">
+          <h1 className="font-semibold text-3xl">Jessica Randall</h1>
+          <p className="mt-[0.6rem] font-bold text-[0.8rem] text-sc-Green mb-[1.4rem]">
             London, United Kingdom
           </p>
-          <p class="text-[0.9rem]">
+          <p className="text-[0.9rem]">
             &ldquo;Frontend developer and avid reader&rdquo;
           </p>
         </div>
-        <div class="my-[1.4rem] mx-auto grid grid-cols-1 grid-rows-5 gap-y-4">
-          <Link
-            href="#"
-            class=" block bg-sc-Grey w-[310px] p-4 text-center text-[0.9rem] font-white rounded-xl font-bold hover:bg-sc-Green hover:text-sc-OffBlack"
-          >
-            Github
-          </Link>
-          <Link
-            href="#"
-            class=" block bg-sc-Grey w-[310px] p-4 text-center text-[0.9rem] font-white rounded-xl font-bold hover:bg-sc-Green hover:text-sc-OffBlack"
-          >
-            Frontend Mentor
-          </Link>
-          <Link
-            href="#"
-            class=" block bg-sc-Grey w-[310px] p-4 text-center text-[0.9rem] font-white rounded-xl font-bold hover:bg-sc-Green hover:text-sc-OffBlack"
-          >
-            LinkedIn
-          </Link>
-          <Link
-            href="#"
-            class=" block bg-sc-Grey w-[310px] p-4 text-center text-[0.9rem] font-white rounded-xl font-bold hover:bg-sc-Green hover:text-sc-OffBlack"
-          >
-            Twitter
-          </Link>
-          <Link
-            href="#"
-            class=" block bg-sc-Grey w-[310px] p-4 text-center text-[0.9rem] font-white rounded-xl font-bold hover:bg-sc-Green hover:text-sc-OffBlack"
-          >
-            Instagram
-          </Link>
+        <div className="my-[1.4rem] mx-auto grid grid-cols-1 gap-y-4">
+          {Links.map((Link) => (
+            <LinkButton key={Link.id} name={Link.name}>
+              Github
+            </LinkButton>
+          ))}
         </div>
       </div>
     </main>
