d53c30e2e830037a42dcb90ce05303fdd49bc06c
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-06-26 20:24:10 +0530

    just bg color change

--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,14 @@ import CardSection from "@/components/main/CardSection"
 
 export default function Home() {
   return (
-    <main className="flex flex-col mx-auto gap-8 bg-almostBlack min-h-screen max-h-max pb-10">
+    <main className="flex flex-col mx-auto gap-8 bg-almostBlack min-h-screen max-h-max pb-4">
       <Header />
       <div className="p-2 sm:px-4 md:px-8 lg:px-16 xl:px-32 grid gap-2 text-almostWhite">
         <h1 className="text-xl md:text-2xl lg:text-3xl  font-extrabold tracking-wide ">
           All Challanges
         </h1>
         <p className="text-xs md:text-sm lg:text-base font-medium">
-          Challanges list from Frontend Mentor which I&apos;ve completed.
+          List of challanges from Frontend Mentor which I&apos;ve completed.
         </p>
       </div>
       <CardSection></CardSection>
