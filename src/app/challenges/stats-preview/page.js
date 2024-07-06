d6c26029017ad269ce1bd2971afb27fb2cbde43c
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-07-06 16:28:14 +0530

    Update page.js
    small screen styling finished

--- src/app/challenges/stats-preview/page.js
+++ src/app/challenges/stats-preview/page.js
@@ -15,36 +15,55 @@ const Stats = [
   },
   {
     name: "queries",
-    stats: "12m+",
+    stats: "12M+",
   },
 ]
 
 const page = () => {
   return (
-    <main className={`${inter.className} text-[15px]`}>
-      <section>
-        <Image
-          src="/stats-preview/image-header-mobile.jpg"
-          width={200}
-          height={200}
-          alt=""
-        ></Image>
-      </section>
-      <section>
-        <h1>Get insights that help your business grow.</h1>{" "}
-        <p>
-          Discover the benefits of data analytics and make better decisions
-          regarding revenue, customer experience, and overall efficiency.
-        </p>{" "}
-        <div>
-          {Stats.map((stat, index) => (
-            <div key={index}>
-              <span>{stat.stats}</span>
-              <span>{stat.name}</span>
-            </div>
-          ))}
-        </div>
-      </section>
+    <main
+      className={`${inter.className} text-[15px] min-h-screen max-h-fit flex flex-col justify-center items-center bg-spc-Very-dark-blue`}
+    >
+      <div className="bg-spc-Dark-desaturated-blue max-w-[350px] flex flex-col justify-center gap-8 mx-8 rounded-lg my-16 ">
+        <section className="">
+          <Image
+            src="/stats-preview/image-header-mobile.jpg"
+            width={200}
+            height={200}
+            alt=""
+            className="w-full object-cover rounded-t-lg"
+          ></Image>
+        </section>
+        <section className="flex flex-col items-center justify-center gap-8">
+          <div className="text-center px-8 flex flex-col gap-4">
+            <h1 className="text-spc-White text-2xl font-bold">
+              Get <span className="text-spc-Soft-violet">insights</span> that
+              help your business grow.
+            </h1>{" "}
+            <p className="text-spc-Slightly-transparent-white-mainPara text-sm leading-6">
+              Discover the benefits of data analytics and make better decisions
+              regarding revenue, customer experience, and overall efficiency.
+            </p>{" "}
+          </div>
+          <div className="flex flex-col gap-6 mb-6 ">
+            {Stats.map((stat, index) => (
+              <div
+                key={index}
+                className="flex flex-col justify-center items-center"
+              >
+                <span className="text-spc-White text-xl font-bold">
+                  {stat.stats}
+                </span>
+                <span
+                  className={`${lexendDeca.className} text-spc-Slightly-transparent-white-statHead uppercase text-[11px] font-normal tracking-wider`}
+                >
+                  {stat.name}
+                </span>
+              </div>
+            ))}
+          </div>
+        </section>
+      </div>
     </main>
   )
 }
