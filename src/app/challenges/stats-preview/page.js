2662e3fb5fea18a267ac48c3cc535e841b9df3b1
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-07-06 15:40:34 +0530

    Update page.js
    all content added now doing styling

--- src/app/challenges/stats-preview/page.js
+++ src/app/challenges/stats-preview/page.js
@@ -1,12 +1,52 @@
 import { Lexend_Deca, Inter } from "next/font/google"
+import Image from "next/image"
 
 const inter = Inter({ subsets: ["latin"], weight: ["400", "700"] })
-const lexenfDeca = Lexend_Deca({ subsets: ["latin"], weight: ["400"] })
+const lexendDeca = Lexend_Deca({ subsets: ["latin"], weight: ["400"] })
+
+const Stats = [
+  {
+    name: "companies",
+    stats: "10k+",
+  },
+  {
+    name: " templates ",
+    stats: "314",
+  },
+  {
+    name: "queries",
+    stats: "12m+",
+  },
+]
 
 const page = () => {
-  return <main>
-    
-  </main>
+  return (
+    <main className={`${inter.className} text-[15px]`}>
+      <section>
+        <Image
+          src="/stats-preview/image-header-mobile.jpg"
+          width={200}
+          height={200}
+          alt=""
+        ></Image>
+      </section>
+      <section>
+        <h1>Get insights that help your business grow.</h1>{" "}
+        <p>
+          Discover the benefits of data analytics and make better decisions
+          regarding revenue, customer experience, and overall efficiency.
+        </p>{" "}
+        <div>
+          {Stats.map((stat, index) => (
+            <div key={index}>
+              <span>{stat.stats}</span>
+              <span>{stat.name}</span>
+            </div>
+          ))}
+        </div>
+      </section>
+    </main>
+  )
 }
 
 export default page
