d3940fa0527e1a3704a933668d517d2c91cf15e3
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-07-17 13:59:04 +0530

    useref used for input

--- src/app/challenges/base-apparel/EmailForm.js
+++ src/app/challenges/base-apparel/EmailForm.js
@@ -1,10 +1,10 @@
 "use client"
 
 import Image from "next/image"
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 const EmailForm = () => {
-  const emailRef = useState(null)
+  const emailRef = useRef(null)
   const [error, setError] = useState("")
 
   const handleSubmit = (e) => {
