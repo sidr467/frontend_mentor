e71e827c8980e04ac24ea198bfdeee2f7ab0b4a3
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-07-17 21:24:51 +0530

    Update SignupForm.js
    working on styling for error

--- src/app/challenges/signup-form/SignupForm.js
+++ src/app/challenges/signup-form/SignupForm.js
@@ -1,35 +1,55 @@
+import Image from "next/image"
+
 const inputs = [
   {
     name: "First name",
+    errorMsg: "First name cannot be empty",
   },
   {
     name: "Last name",
+    errorMsg: "Last name cannot be empty",
   },
   {
     name: "Email Address",
+    errorMsg: "Looks like this not an Email",
   },
   {
     name: "Password",
+    errorMsg: "Password cannot be empty",
   },
 ]
 
 const SignupForm = () => {
   return (
-    <div className="bg-white flex items-start justify-center flex-col gap-4 p-6 md:p-12  rounded-lg ">
-      <div className="flex flex-col gap-6 w-full">
+    <div className="bg-white flex items-start justify-center flex-col gap-4 px-6 py-2 md:p-12  rounded-lg ">
+      <form className="flex flex-col gap-4 w-full">
         {inputs.map((input, index) => (
-          <input
-            key={index}
-            type="text"
-            placeholder={input.name}
-            // value={input.name}
-            className="ring-1 ring-opacity-30 focus:ring-icsf-dark-blue focus:outline-none focus:opacity-90 ring-icsf-dark-blue w-full h-12 rounded-sm px-4 text-icsf-dark-blue text-sm font-semibold"
-          />
+          <div key={index} className="relative flex flex-col gap-1">
+            <input
+              type="text"
+              placeholder={input.name}
+              required
+              className=" placeholder:text-icsf-dark-blue ring-1 ring-opacity-30 focus:ring-icsf-dark-blue focus:outline-none focus:opacity-90 ring-icsf-dark-blue w-full h-12 rounded-sm px-4 text-icsf-dark-blue text-sm font-semibold"
+            />
+            <Image
+              src="/signup-form/icon-error.svg"
+              width={30}
+              height={30}
+              alt="Error-Icon"
+              className="absolute right-2 top-3 w-6"
+            ></Image>
+            <div className="text-xs text-end text-icsf-red italic font-normal">
+              <p>{input.errorMsg}</p>
+            </div>
+          </div>
         ))}
-      </div>
-      <div className="shadow-[0_4px_0_0_rgba(46,184,130,1)] bg-icsf-green w-full h-12 rounded-md flex items-center justify-center uppercase text-white text-sm font-semibold tracking-wider">
-        Claim your free trial
-      </div>
+        <button
+          type="submit"
+          className="shadow-[0_4px_0_0_rgba(46,184,130,1)] bg-icsf-green w-full h-12 rounded-md flex items-center justify-center uppercase text-white text-sm font-semibold tracking-wider"
+        >
+          Claim your free trial
+        </button>
+      </form>
       <div className="text-xs text-center w-full px-2 leading-6 ">
         <p className="text-icsf-grayish-blue font-medium">
           By clicking the button, you are agreeing to our{" "}
