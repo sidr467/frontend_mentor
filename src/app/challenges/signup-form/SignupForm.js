b1a65a9861b32dd194229c808f2ea4530a4ea51f
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-07-18 00:33:11 +0530

    Update SignupForm.js
    finished with validation

--- src/app/challenges/signup-form/SignupForm.js
+++ src/app/challenges/signup-form/SignupForm.js
@@ -1,46 +1,103 @@
+"use client"
+
 import Image from "next/image"
+import { useRef, useState } from "react"
 
 const inputs = [
   {
-    name: "First name",
+    placeholder: "First name",
+    name: "firstName",
+    type: "text",
     errorMsg: "First name cannot be empty",
   },
   {
-    name: "Last name",
+    placeholder: "Last name",
+    name: "lastName",
+    type: "text",
+
     errorMsg: "Last name cannot be empty",
   },
   {
-    name: "Email Address",
-    errorMsg: "Looks like this not an Email",
+    placeholder: "Email Address",
+    name: "email",
+    type: "text",
+    errorMsg: "Email cannot be empty",
   },
   {
-    name: "Password",
+    placeholder: "Password",
+    name: "password",
+    type: "password",
     errorMsg: "Password cannot be empty",
   },
 ]
 
 const SignupForm = () => {
+  const formRef = useRef({})
+  const [formErrors, setFormErrors] = useState({})
+
+  const formValidation = () => {
+    const errors = {}
+    inputs.forEach((input) => {
+      const value = formRef.current[input.name].value
+
+      if (!value) {
+        errors[input.name] = input.errorMsg
+      } else if (input.name === "email") {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+        if (!emailRegex.test(value)) {
+          errors[input.name] = "Looks like this is not an Email"
+        }
+      }
+    })
+    return errors
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const errors = formValidation()
+    if (Object.keys(errors).length === 0) {
+      console.log("Submission Successfull")
+      setFormErrors({})
+      Object.values(formRef.current).forEach((ref) => (ref.value = ""))
+    } else {
+      setFormErrors(errors)
+    }
+  }
+
   return (
-    <div className="bg-white flex items-start justify-center flex-col gap-4 px-6 py-2 md:p-12  rounded-lg ">
-      <form className="flex flex-col gap-4 w-full">
+    <div className="bg-white flex items-start justify-center flex-col gap-4 p-6 md:p-12  rounded-lg ">
+      <form
+        className="flex flex-col gap-4 w-full"
+        action=""
+        noValidate
+        onSubmit={handleSubmit}
+      >
         {inputs.map((input, index) => (
           <div key={index} className="relative flex flex-col gap-1">
             <input
-              type="text"
-              placeholder={input.name}
+              type={input.type}
+              placeholder={input.placeholder}
+              name={input.name}
               required
-              className=" placeholder:text-icsf-dark-blue ring-1 ring-opacity-30 focus:ring-icsf-dark-blue focus:outline-none focus:opacity-90 ring-icsf-dark-blue w-full h-12 rounded-sm px-4 text-icsf-dark-blue text-sm font-semibold"
+              ref={(el) => (formRef.current[input.name] = el)}
+              className={`${
+                formErrors[input.name] ? "ring-icsf-red ring-2" : ""
+              } placeholder:text-icsf-dark-blue placeholder:font-bold placeholder:opacity-60 font-bold ring-1 ring-opacity-30 focus:ring-icsf-dark-blue focus:outline-none focus:opacity-90 ring-icsf-dark-blue w-full h-12 rounded-sm px-4 text-icsf-dark-blue text-sm `}
             />
-            <Image
-              src="/signup-form/icon-error.svg"
-              width={30}
-              height={30}
-              alt="Error-Icon"
-              className="absolute right-2 top-3 w-6"
-            ></Image>
-            <div className="text-xs text-end text-icsf-red italic font-normal">
-              <p>{input.errorMsg}</p>
-            </div>
+            {formErrors[input.name] && (
+              <>
+                <Image
+                  src="/signup-form/icon-error.svg"
+                  width={30}
+                  height={30}
+                  alt="Error-Icon"
+                  className="absolute right-2 top-3 w-6"
+                ></Image>
+                <div className="text-xs text-end text-icsf-red italic font-normal">
+                  <p>{formErrors[input.name]}</p>
+                </div>
+              </>
+            )}
           </div>
         ))}
         <button
