a72f154ace907667a03991dd74d4bbdbf7743b45
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-07-18 20:56:54 +0530

    finished with challenge

--- src/app/challenges/ping-coming-soon/EmailForm.js
+++ src/app/challenges/ping-coming-soon/EmailForm.js
@@ -1,17 +1,55 @@
+"use client"
+
+import { useRef, useState } from "react"
+
 const EmailForm = () => {
+  const emailRef = useRef(null)
+  const [error, setError] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const email = emailRef.current.value
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+    if (email === "") {
+      setError("Whoops! It looks like you forgot to add your email")
+      return
+    } else if (!emailRegex.test(email)) {
+      setError("Please provide a valid email address")
+      return
+    }
+
+    setError("")
+    emailRef.current.value = ""
+  }
+
   return (
     <div className="flex items-center justify-center">
       <form
         action=""
         noValidate
         className="flex items-center justify-center flex-col md:flex-row gap-2 w-full"
+        onSubmit={handleSubmit}
       >
-        <input
-          type="text"
-          placeholder="Your Email address..."
-          className="ring-1 rounded-full py-3 px-8 w-full md:w-96 ring-pcs-pale-blue text-sm "
-        />
-        <button type="submit" className="rounded-full py-3 w-full md:w-52 bg-pcs-blue text-white text-sm font-semibold shadow-xl">
+        <div className="w-full flex items-center justify-center gap-2 flex-col md:relative">
+          <input
+            type="email"
+            placeholder="Your Email address..."
+            ref={emailRef}
+            className={`${
+              error ? "ring-pcs-light-red" : ""
+            } ring-1 rounded-full py-3 px-8 w-full md:w-96 ring-pcs-pale-blue text-sm  `}
+          />
+          {error && (
+            <p className="text-xs italic text-pcs-light-red mb-4 md:mb-0 md:absolute left-4 -bottom-6 ">
+              {error}
+            </p>
+          )}
+        </div>
+        <button
+          type="submit"
+          className="rounded-full py-3 w-full md:w-52 bg-pcs-blue hover:opacity-80 text-white text-sm font-semibold shadow-xl"
+        >
           Notify me
         </button>
       </form>
--- src/app/challenges/ping-coming-soon/IconsComp.js
+++ src/app/challenges/ping-coming-soon/IconsComp.js
@@ -2,13 +2,19 @@ import { FaTwitter, FaInstagram, FaFacebookF } from "react-icons/fa"
 
 const Icons = [
   {
-    icon: <FaFacebookF className="fill-pcs-blue hover:fill-white" size={18} />,
+    icon: (
+      <FaFacebookF className="fill-pcs-blue group-hover:fill-white" size={18} />
+    ),
   },
   {
-    icon: <FaTwitter className="fill-pcs-blue hover:fill-white" size={18} />,
+    icon: (
+      <FaTwitter className="fill-pcs-blue group-hover:fill-white" size={18} />
+    ),
   },
   {
-    icon: <FaInstagram className="fill-pcs-blue hover:fill-white" size={18} />,
+    icon: (
+      <FaInstagram className="fill-pcs-blue group-hover:fill-white" size={18} />
+    ),
   },
 ]
 
@@ -16,12 +22,12 @@ const IconsComp = () => {
   return (
     <div className="flex gap-4">
       {Icons.map((data, index) => (
-        <div
+        <button
           key={index}
-          className="rounded-full ring-1 p-2 hover:bg-pcs-blue ring-opacity-40 ring-pcs-gray flex items-center justify-center"
+          className="rounded-full ring-1 p-2 group hover:bg-pcs-blue ring-opacity-40 ring-pcs-gray flex items-center justify-center"
         >
-          <button>{data.icon}</button>
-        </div>
+          {data.icon}
+        </button>
       ))}
     </div>
   )
