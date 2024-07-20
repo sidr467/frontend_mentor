74c3d3999034eb1e005d605189659a281e95677e
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-07-20 23:17:27 +0530

    Update page.js
    finished styling page but it still needs restyling of some things

--- src/app/challenges/huddle-landing-page/page.js
+++ src/app/challenges/huddle-landing-page/page.js
@@ -2,15 +2,14 @@ import { Poppins, Open_Sans } from "next/font/google"
 import Image from "next/image"
 import IconsComp from "./IconsComp"
 
-//fonts
+// fonts
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "600"] })
 const openSans = Open_Sans({ subsets: ["latin"], weight: ["400"] })
 
-const page = () => {
+const Page = () => {
   return (
-    <div className="min-h-screen max-h-fit bg-hlp-Violet flex justify-center py-2
-    ">
-      <main className="w-full h-full flex justify-center items-center flex-col gap-12 md:gap-20 px-8 lg:px-24">
+    <div className="min-h-screen bg-hlp-Violet bg-[url('/huddle-landing-page/bg-mobile.svg')] md:bg-[url('/huddle-landing-page/bg-desktop.svg')] bg-repeat-x md:bg-no-repeat bg-cover md:bg-contain flex justify-center py-12">
+      <main className="flex justify-center items-center flex-col gap-12 md:gap-20 px-8 lg:px-24">
         <div className="place-self-start">
           <Image
             src="/huddle-landing-page/logo.svg"
@@ -18,26 +17,26 @@ const page = () => {
             height={150}
             alt="Logo-Image"
             className="md:w-48"
-          ></Image>
+          />
         </div>
-        <div className="flex gap-12 flex-col md:flex-row">
-          <div className="flex items-center justify-center md:justify-start flex-shrink-0">
+        <div className="flex gap-12 flex-col md:flex-row w-full h-full">
+          <div className="flex items-center justify-center md:justify-start w-full h-full">
             <Image
               src="/huddle-landing-page/illustration-mockups.svg"
               width={320}
               height={320}
               alt="illustration"
-              className="md:max-w-[550px] lg:max-w-[720px] w-full h-full object-cover"
-            ></Image>
+              className="md:max-w-[550px] lg:max-w-[720px] w-full h-full object-contain"
+            />
           </div>
-          <div className="flex items-center justify-center md:items-start flex-col gap-6 md:gap-10">
+          <div className="flex items-center justify-center md:items-start flex-col gap-6 md:gap-10 w-full h-full">
             <h1
-              className={` ${poppins.className} text-center md:text-start text-2xl md:text-5xl text-white font-semibold`}
+              className={`${poppins.className} text-center md:text-start text-2xl md:text-5xl text-white font-semibold`}
             >
               Build The Community Your Fans Will Love
             </h1>
             <p
-              className={`${openSans.className} text-white font-normal -mx-1 md:text-xl text-center md:text-start`}
+              className={`${openSans.className} text-white font-normal -mx-1 md:mx-0 md:text-lg text-center md:text-start`}
             >
               Huddle re-imagines the way we build communities. You have a voice,
               but so does your audience. Create connections with your users as
@@ -56,4 +55,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Page
