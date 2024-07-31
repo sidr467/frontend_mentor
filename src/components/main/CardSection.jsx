c0bd5a076478323d1d7aca1c372424807cd70d05
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-07-31 20:11:36 +0530

    Update CardSection.jsx
    last time reverse didnt work in deploy

--- src/components/main/CardSection.jsx
+++ src/components/main/CardSection.jsx
@@ -4,16 +4,18 @@ import { challenges } from "./Challanges"
 function CardSection() {
   return (
     <div className="p-2 sm:px-4 md:px-8 lg:px-16 xl:px-32 grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3 auto-rows-auto xl:gap-12 mx-auto">
-      {challenges.reverse().map((challenge) => (
-        <Cards
-          key={challenge}
-          title={challenge.title}
-          link={challenge.link}
-          difficulty={challenge.difficulty}
-          difficultyText={challenge.difficultyText}
-          image={challenge.image}
-        />
-      ))}
+      {challenges
+        .map((challenge) => (
+          <Cards
+            key={challenge}
+            title={challenge.title}
+            link={challenge.link}
+            difficulty={challenge.difficulty}
+            difficultyText={challenge.difficultyText}
+            image={challenge.image}
+          />
+        ))
+        .reverse()}
     </div>
   )
 }
