5866932da1f989e1b20ae8f248104241a8231965
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-07-31 20:02:17 +0530

    Update CardSection.jsx
    just reverse challenges order to show latest ones

--- src/components/main/CardSection.jsx
+++ src/components/main/CardSection.jsx
@@ -4,7 +4,7 @@ import { challenges } from "./Challanges"
 function CardSection() {
   return (
     <div className="p-2 sm:px-4 md:px-8 lg:px-16 xl:px-32 grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3 auto-rows-auto xl:gap-12 mx-auto">
-      {challenges.map((challenge) => (
+      {challenges.reverse().map((challenge) => (
         <Cards
           key={challenge}
           title={challenge.title}
