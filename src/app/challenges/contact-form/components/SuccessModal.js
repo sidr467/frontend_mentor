d5d107664da55c25055ad7742019521021164bc4
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-08-10 16:52:46 +0530

    Update SuccessModal.js

--- src/app/challenges/contact-form/components/SuccessModal.js
+++ src/app/challenges/contact-form/components/SuccessModal.js
@@ -2,7 +2,7 @@ import { FaCheck } from "react-icons/fa6"
 
 const SuccessModal = () => {
   return (
-    <div className="bg-contactform-Grey900 text-contactform-White py-6 px-8 rounded-lg flex flex-col gap-3 absolute bottom-0 md:-top-6">
+    <div className="bg-contactform-Grey900 text-contactform-White py-6 px-8 rounded-lg flex flex-col gap-3 absolute -top-6 ">
       <h3 className="flex items-center gap-4 text-lg font-bold ">
         <FaCheck className="ring-1 p-0.5 ring-contactform-White rounded-full " />
         <span>Message Sent!</span>
